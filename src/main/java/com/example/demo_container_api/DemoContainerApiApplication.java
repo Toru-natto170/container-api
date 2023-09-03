a854e5eb2d49fb3637def539a0b01322579ac771
Natto 710 <f-toru0924@outlook.jp>
2023-09-03 23:00:19 +0900

    initial commit

---
+++ src/main/java/com/example/demo_container_api/DemoContainerApiApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo_container_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoContainerApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoContainerApiApplication.class, args);
+	}
+
+}
